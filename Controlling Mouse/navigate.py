a50e17f0a5bd45b8086e429abff3d9a1d42286d5
Ishank Sharma <45262592+ishank-dev@users.noreply.github.com>
2019-10-14 22:08:25 +0530

    Update navigate.py

--- Controlling Mouse/navigate.py
+++ Controlling Mouse/navigate.py
@@ -1,1 +1,13 @@
-Work in progress
+import pyautogui
+pyautogui.size() # to get the dimensions of the screen
+width,height = pyautogui.size() # store height and width in respective variables
+pyautogui.position() # returns the current mouse pointer locaton
+pyautogui.moveTo(10,10,duration = 1.5)# move the pointer to the provided coordinates
+pyautogui.moveRel(200,0,duration = 1.5)# move the pointer to the provided coordinates( in pixels)
+pyautogui.click(339,38) # click on a specific coordinate
+pyautogui.doubleClick()
+pyautogui.middleClick()
+pyautogui.rightClick()
+pyautogui.dragTo(10,10,duration = 1.5)# drag the pointer to the provided coordinates
+
+pyautogui.displayMousePosition() #real time mouse pointer location 
